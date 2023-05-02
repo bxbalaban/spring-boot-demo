3e4c459b69fe1694058ab7cfb369b8df4fd13939
bxbalaban <balabanbsr@gmail.com>
2023-05-02 22:13:49 +0300

    created first post end-point

--- src/main/java/com/bxbalaban/demo/api/PersonController.java
+++ src/main/java/com/bxbalaban/demo/api/PersonController.java
@@ -1,10 +1,18 @@
 package com.bxbalaban.demo.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.bxbalaban.demo.model.Person;
 import com.bxbalaban.demo.service.PersonService;
 
+@RestController // we can provide end points to clients and consume
+@RequestMapping("api/v1/person")
+
+// post get delete must be available as a rest controller
 public class PersonController {
     private final PersonService personService;
 
@@ -13,7 +21,8 @@ public class PersonController {
         this.personService = personService;
     }
 
-    public void addPerson(Person person) {
+    @PostMapping // will serve as a post method
+    public void addPerson(@RequestBody Person person) {
         personService.addPerson(person);
     }
 
