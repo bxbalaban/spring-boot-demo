a8a742130e776ca0553af925ac82935113f1c507
bxbalaban <balabanbsr@gmail.com>
2023-05-03 23:15:31 +0300

    access to postgresql methods added

--- src/main/java/com/bxbalaban/demo/dao/PersonDataAccessService.java
+++ src/main/java/com/bxbalaban/demo/dao/PersonDataAccessService.java
@@ -1,9 +1,15 @@
 package com.bxbalaban.demo.dao;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import com.bxbalaban.demo.model.Person;
@@ -11,10 +17,27 @@ import com.bxbalaban.demo.model.Person;
 @Repository("postgres")
 public class PersonDataAccessService implements PersonDao {
 
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public PersonDataAccessService(JdbcTemplate jdbcTemplate){
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     @Override
     public List<Person> selectAllPeople() {
-        return List.of(new Person(UUID.randomUUID(), "FROM POSTGRES DB"));
+        final String sqlw = "SELECT id , name FROM person";
+      
+            Object people = jdbcTemplate.query(sqlw,(resultSet,i)-> {
+                String name = resultSet.getString("name");
+                UUID id = UUID.fromString(resultSet.getString("id"));
+                return new Person(id,name);
+            });
+            return (List<Person>) people;
+        //return List.of(new Person(UUID.randomUUID(), "FROM POSTGRES DB"));
     }
+
+   
 
     @Override
     public int insertPerson(UUID id, Person person) {
@@ -24,8 +47,13 @@ public class PersonDataAccessService implements PersonDao {
 
     @Override
     public Optional<Person> selectPersonById(UUID id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'selectPersonById'");
+        String sql = "SELECT id, name FROM person WHERE id = ?";
+        Person person = jdbcTemplate.queryForObject(sql, new Object[]{id}, (resultSet,i)-> {
+            String name = resultSet.getString("name");
+            UUID personId = UUID.fromString(resultSet.getString("id"));
+            return new Person(personId,name);
+        });
+        return Optional.ofNullable(person);
     }
 
     @Override
